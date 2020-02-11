34cc4e71d3539966ca4e5cda30ec3fca7ffac9f2
bet476 <60711188+bet476@users.noreply.github.com>
2020-02-11 12:05:13 -0300

    Delete atest.js

--- atest.js
+++
@@ -1,9 +0,0 @@
- <source src="https://live-oiplay.online.go.oi.com.br/live-cdn/fx.smil/chunklist_b528441_sleng.m3u8" type="application/x-mpegURL" res="180" label="180">
-
- <source src="https://live-oiplay.online.go.oi.com.br/live-cdn/fx.smil/chunklist_b664685_sleng.m3u8" type="application/x-mpegURL" res="360" label="360">
-
- <source src="https://live-oiplay.online.go.oi.com.br/live-cdn/fx.smil/chunklist_b1101581_sleng.m3u8" type="application/x-mpegURL" res="404" label="404">
-
- <source src="https://live-oiplay.online.go.oi.com.br/live-cdn/fx.smil/chunklist_b1431167_sleng.m3u8" type="application/x-mpegURL" res="480" label="480">
-
- <source src="https://cdn.jsdelivr.net/gh/bet476/rtm/espnbra/master.mpd?" type="application/x-mpegURL" res="AUTO" label="AUTO">
